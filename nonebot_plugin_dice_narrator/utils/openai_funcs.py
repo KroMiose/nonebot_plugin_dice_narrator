fbc7177df17c3322902f1dfcac2565e8a2010032
KroMiose <li_xiangff@163.com>
2024-03-12 16:55:49 +0800

    放宽依赖版本需求

--- nonebot_plugin_dice_narrator/utils/openai_funcs.py
+++ nonebot_plugin_dice_narrator/utils/openai_funcs.py
@@ -1,14 +1,22 @@
-from typing import Dict, List, Literal, Optional, Tuple
+from math import log
+from typing import Dict, List, Optional, Tuple
 
 import httpx
 import openai
+import pkg_resources
 from nonebot import logger
-from openai import AsyncOpenAI
-from pydantic import BaseModel
 
 from nonebot_plugin_dice_narrator.config import config
 
-openai.base_url = config.OPENAI_BASE_URL
+__openai_version = pkg_resources.get_distribution("openai").version
+
+if __openai_version <= "0.28.0":
+    openai.api_base = config.OPENAI_BASE_URL  # type: ignore
+else:
+    from openai import AsyncOpenAI  # type: ignore
+
+    openai.base_url = config.OPENAI_BASE_URL  # type: ignore
+
 openai_key_idx = 0
 
 
@@ -50,10 +58,33 @@ async def gen_chat_response_text(
     temperature: float = 0,
     frequency_penalty: float = 0.2,
     presence_penalty: float = 0.2,
-    top_p=1,
+    top_p: float = 1,
+    stop_words: Optional[List[str]] = None,
+    max_tokens: Optional[int] = None,
     api_key: Optional[str] = None,
 ) -> Tuple[str, int]:
     """生成聊天回复内容"""
+
+    if __openai_version <= "0.28.0":
+        openai.api_key = api_key
+
+        res = openai.ChatCompletion.create(  # type: ignore
+            model=config.CHAT_MODEL,
+            messages=messages,
+            temperature=temperature,
+            top_p=top_p,
+            frequency_penalty=frequency_penalty,
+            presence_penalty=presence_penalty,
+            max_tokens=max_tokens,
+            stop=stop_words,
+        )
+        logger.debug(f"Chat response: {res}")
+
+        output = res.choices[0].message.content  # type: ignore
+        token_consumption = res.usage.total_tokens  # type: ignore
+        logger.info(f"请求消耗: {token_consumption} tokens")
+        assert output, "Chat response is empty"
+        return output, token_consumption
 
     client = AsyncOpenAI(
         api_key=api_key,
@@ -74,6 +105,8 @@ async def gen_chat_response_text(
         frequency_penalty=frequency_penalty,
         presence_penalty=presence_penalty,
         top_p=top_p,
+        max_tokens=max_tokens,
+        stop=stop_words,
     )
 
     output = res.choices[0].message.content
