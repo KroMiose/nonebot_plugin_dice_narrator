296e62e8df64801f64935231e6bb0ebbe53b2aa0
KroMiose <li_xiangff@163.com>
2024-03-14 10:08:02 +0800

    fix: load-test workflow

---
+++ run_bot.py
@@ -0,0 +1,18 @@
+import nonebot
+from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
+
+nonebot.init()
+
+driver = nonebot.get_driver()
+driver.register_adapter(ONEBOT_V11Adapter)
+
+
+nonebot.load_from_toml("pyproject.toml")
+
+
+def main():
+    nonebot.run()
+
+
+if __name__ == "__main__":
+    main()
